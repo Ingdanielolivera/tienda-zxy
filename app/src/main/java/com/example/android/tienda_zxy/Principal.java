148399219ba0156f276b635754c3a2ace51af14d
“Daniel” <daniel@olivera.com>
2017-05-06 15:14:55 -0500

    Aplicacion finalizada con todas las validaciones

--- app/src/main/java/com/example/android/tienda_zxy/Principal.java
+++ app/src/main/java/com/example/android/tienda_zxy/Principal.java
@@ -1,8 +1,10 @@
 package com.example.android.tienda_zxy;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -27,9 +29,9 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        cajaPrecio = (TextView)findViewById(R.id.txtresultado);
-        cajaCantidad = (EditText)findViewById(R.id.txtcantidad);
-        cajaUnitario =(TextView)findViewById(R.id.txtunit);
+        cajaPrecio = (TextView) findViewById(R.id.txtresultado);
+        cajaCantidad = (EditText) findViewById(R.id.txtcantidad);
+        cajaUnitario = (TextView) findViewById(R.id.txtunit);
 
         comboOpcionesSexo = (Spinner) findViewById(R.id.cmbsexo);
         opcionesSexo = this.getResources().getStringArray(R.array.opcionesSexo);
@@ -55,87 +57,109 @@ public class Principal extends AppCompatActivity {
 
 
     public void Precio(View v) {
-        int opsexo, optzapa, opmarca,cant,res=0;
+        int opsexo, optzapa, opmarca, cant, res = 0;
 
-        opsexo = comboOpcionesSexo.getSelectedItemPosition();
-        optzapa = comboOpcionesTzapa.getSelectedItemPosition();
-        opmarca = comboOpcionesMarca.getSelectedItemPosition();
-        cant = Integer.parseInt(cajaCantidad.getText().toString());
+        if (validar()) {
+            opsexo = comboOpcionesSexo.getSelectedItemPosition();
+            optzapa = comboOpcionesTzapa.getSelectedItemPosition();
+            opmarca = comboOpcionesMarca.getSelectedItemPosition();
+            cant = Integer.parseInt(cajaCantidad.getText().toString());
 
-        switch (opsexo) {
-            case 0:
-                    switch (optzapa){
+            switch (opsexo) {
+
+                case 0:
+                    switch (optzapa) {
                         case 0:
-                            switch (opmarca){
+                            switch (opmarca) {
                                 case 0:
-                                    res=cant*120000;
+                                    res = cant * 120000;
                                     cajaUnitario.setText("$120.000");
                                     break;
                                 case 1:
-                                    res=cant*140000;
+                                    res = cant * 140000;
                                     cajaUnitario.setText("$140.000");
                                     break;
                                 case 2:
-                                    res=cant*130000;
+                                    res = cant * 130000;
                                     cajaUnitario.setText("$130.000");
                                     break;
                             }
                             break;
                         case 1:
-                            switch (opmarca){
+                            switch (opmarca) {
                                 case 0:
-                                    res=cant*50000;
+                                    res = cant * 50000;
                                     cajaUnitario.setText("$50.000");
                                     break;
                                 case 1:
-                                    res=cant*80000;
+                                    res = cant * 80000;
                                     cajaUnitario.setText("$80.000");
                                     break;
                                 case 2:
-                                    res=cant*100000;
+                                    res = cant * 100000;
                                     cajaUnitario.setText("$100.000");
                                     break;
                             }
                     }
-                break;
-            case 1:
-                switch (optzapa){
-                    case 0:
-                        switch (opmarca){
-                            case 0:
-                                res=cant*100000;
-                                cajaUnitario.setText("$100.000");
-                                break;
-                            case 1:
-                                res=cant*130000;
-                                cajaUnitario.setText("$130.000");
-                                break;
-                            case 2:
-                                res=cant*110000;
-                                cajaUnitario.setText("$110.000");
-                                break;
-                        }
-                        break;
-                    case 1:
-                        switch (opmarca){
-                            case 0:
-                                res=cant*60000;
-                                cajaUnitario.setText("$60.000");
-                                break;
-                            case 1:
-                                res=cant*70000;
-                                cajaUnitario.setText("$70.000");
-                                break;
-                            case 2:
-                                res=cant*120000;
-                                cajaUnitario.setText("$120.000");
-                                break;
-                        }
-                }
+                    break;
+                case 1:
+                    switch (optzapa) {
+                        case 0:
+                            switch (opmarca) {
+                                case 0:
+                                    res = cant * 100000;
+                                    cajaUnitario.setText("$100.000");
+                                    break;
+                                case 1:
+                                    res = cant * 130000;
+                                    cajaUnitario.setText("$130.000");
+                                    break;
+                                case 2:
+                                    res = cant * 110000;
+                                    cajaUnitario.setText("$110.000");
+                                    break;
+                            }
+                            break;
+                        case 1:
+                            switch (opmarca) {
+                                case 0:
+                                    res = cant * 60000;
+                                    cajaUnitario.setText("$60.000");
+                                    break;
+                                case 1:
+                                    res = cant * 70000;
+                                    cajaUnitario.setText("$70.000");
+                                    break;
+                                case 2:
+                                    res = cant * 120000;
+                                    cajaUnitario.setText("$120.000");
+                                    break;
+                            }
+                    }
+                    break;
+            }
+            cajaPrecio.setText("$" + res);
 
-
-                break;
+            View view = this.getCurrentFocus();
+            view.clearFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
         }
-        cajaPrecio.setText("$"+res);
+    }
+
+    public boolean validar() {
+        if (cajaCantidad.getText().toString().isEmpty()) {
+            cajaCantidad.setError(this.getResources().getString(R.string.error_cantidad));
+            cajaCantidad.requestFocus();
+            return false;
+        }
+        if (Integer.parseInt(cajaCantidad.getText().toString()) == 0) {
+            cajaCantidad.setError(this.getResources().getString(R.string.error_cantidad_zero));
+            cajaCantidad.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
